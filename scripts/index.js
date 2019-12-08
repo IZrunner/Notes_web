2567a5a358cf6574a0d0e41a624652ea50d17255
IZrunner <masterchefchinatown@gmail.com>
2019-12-08 23:42:53 +0200

    added delete functionality
    added delete buttons, but they don't work properly : it's possible to delete element only by clicking the first button: elements are deleted starting from the first one. they are deleted from client as button is pressed, but deleted from the bottom to top of the list on the server.
    to-do : fix bug and polish design

--- scripts/index.js
+++ scripts/index.js
@@ -35,7 +35,10 @@ const setupNotes = (data) => {
       const note = doc.data();
       const li = `
         <li>
-          <div class="collapsible-header grey lighten-4"> ${note.title} </div>
+          <div class="collapsible-header grey lighten-4"> ${note.title} 
+            <div class="delete-button"><a class="waves-effect waves-light btn">
+            <i class="material-icons right">delete</i>DELETE</a></div>
+          </div>
           <div class="collapsible-body white"> ${note.content} </div>
         </li>
       `;
@@ -45,8 +48,45 @@ const setupNotes = (data) => {
   } else {
     noteList.innerHTML = '<h5 class="center-align">Login to view notes</h5>';
   }
-  
 
+  // db.collection('cafes').orderBy('title').get().then(snapshot => {
+  //   snapshot.docs.forEach(doc => {
+  //     let li = document.querySelector("#notes-list ul li");
+  //     li.setAttribute('note-id', doc.id);
+  //   });
+  // });
+
+  data.forEach(doc => {
+    let li = document.querySelector("#notes-list ul li");
+    li.setAttribute('note-id', doc.id);
+  });
+
+  //delete notes
+  const deleteNote = document.querySelector("#notes-list ul li div div");
+  deleteNote.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    if(deleteNote.className == 'delete-button'){
+      let li = e.target.parentElement.parentElement;
+      li.parentNode.removeChild(li);
+    } else {
+      console.log("you passed null to the if statement. you didn't delete the note from client");
+    }
+
+    console.log("now you're trying to delete it from database");
+    console.log(e.target.parentElement.getAttribute);
+    let id = e.target.parentElement.getAttribute('note-id');
+    console.log(id);
+    if(id !== null) {
+      db.collection('notes').doc(id).delete().then(function() {
+        console.log("Document successfully deleted!");
+        }).catch(function(error) {
+          console.error("Error removing document: ", error);
+        });
+    } else {
+      console.log("your id is for some reason a null");
+    }
+  });
 };
 
 // setup materialize components
